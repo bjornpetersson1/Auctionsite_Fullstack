ec815bf301ad08c259e5670c4e4435751bf7c6ff
bjornpetersson1 <bjornpetersson1@live.se>
2026-05-06 13:59:14 +0200

    Accesstoken is generated and returned

---
+++ Auctionsite_Backend/Auctionsite_Backend/Auctionsite_Backend/Core/Service/JWTservice.cs
@@ -0,0 +1,46 @@
+﻿using Auctionsite_Backend.Core.Interface;
+using Auctionsite_Backend.Data.DTO;
+using Auctionsite_Backend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auctionsite_Backend.Core.Service
+{
+    public class JWTservice : IJWTservice
+    {
+        private readonly IConfiguration _config;
+
+        public JWTservice(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateAccessToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtKey"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(2),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken(string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Auctionsite_Backend/Auctionsite_Backend/Auctionsite_Backend/Data/DTO/LoginResponseDTO.cs
+++ Auctionsite_Backend/Auctionsite_Backend/Auctionsite_Backend/Data/DTO/LoginResponseDTO.cs
@@ -2,7 +2,11 @@
 {
     public class LoginResponseDTO
     {
-        public string ResponseMessage { get; set; }
         public bool LoginSuccess { get; set; }
+        public string ResponseMessage { get; set; }
+        public int? Id { get; set; }
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+        public string? AccessToken { get; set; }
     }
 }
